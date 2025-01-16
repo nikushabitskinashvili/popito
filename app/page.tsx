ce10056c6416cc531eeca9cf328a23aeae09aa68
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2025-01-16 15:57:27 +0400

    feat: create movie dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import GenreDropDown from "./components/GenreDropDown/GenreDropDown";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
-  return <div className={styles.page}>
-    <GenreDropDown></GenreDropDown>
-  </div>;
+  return <div className={styles.page}></div>;
 }
