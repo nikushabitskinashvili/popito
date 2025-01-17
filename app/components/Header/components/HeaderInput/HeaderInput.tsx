6bf6df776638bfc24e8ac6568919d17da680ee25
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2025-01-17 17:32:14 +0400

    feat: create reusable header

---
+++ app/components/Header/components/HeaderInput/HeaderInput.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './HeaderInput.module.scss';
+import Image from 'next/image';
+import { headerInputProps } from '../Interface/Interface';
+
+const HeaderInput: React.FC<headerInputProps> = ({
+  type = 'text',
+  placeholder = 'Search',
+  value,
+  name,
+  onChange,
+}) => {
+  return (
+    <div className={styles.inputWrapper}>
+      <div className={styles.container}>
+        <Image
+          src={'/icons/search.svg'}
+          alt={'search'}
+          width={20}
+          height={20}
+        />
+        <input
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          name={name}
+          onChange={onChange}
+          className={styles.input}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderInput;
---
+++ app/components/Header/components/headerdummy/header-dummy.ts
@@ -0,0 +1,5 @@
+export const navLinks = [
+  { title: 'Home', pathname: '/', key: '/' },
+  { title: 'Movies', pathname: '/movies', key: '/movies' },
+  { title: 'List', pathname: '/list', key: '/list' },
+];
