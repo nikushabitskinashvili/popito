170e48d0ec8870e8b5536029d6383533ee51ea08
Irakli Pankvelashvili <irakli.pankvelashvili@gmail.com>
2025-01-17 18:10:52 +0400

    add  movieCard

---
+++ app/ts/interfaces.d.ts
@@ -0,0 +1,15 @@
+interface ganre{
+    id:number
+    title:string
+}
+
+interface MovieCard{
+    id:number,
+    title:string,
+    year:number,
+    rating:number,
+    ganres:ganre[],
+    movieImg:string,
+    duration:number
+}
+
